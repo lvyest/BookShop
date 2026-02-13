1a862f6a41e48797298693cb21d486b411651ebf
KAYOUNG KIM <lvyest.dev@gmail.com>
2026-02-13 23:43:35 +0900

    feat: 프로젝트 초기 세팅

---
+++ routes/books.js
@@ -0,0 +1,23 @@
+ const express = require('express');
+ const router = express.Router();
+
+ //post때문에 필요 
+ router.use(express.json()); 
+
+ //전체 도서 조회
+ router.get('/', (req, res)=> {
+    res.json('전체 도서 조회');
+ });
+
+ //개별 도서 조회
+ router.get('/:id', (req, res)=> {
+    res.json('개별 도서 조회');
+ });
+
+ //카테고리별 도서 목록 조회
+ router.get('/', (req, res) => {
+    res.json('카테고리별 도서 목록 조회');
+ });
+
+
+ module.exports = router;
