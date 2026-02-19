3ceb625d6b27b2ff48af9b691e3f80b28b2d54bb
KAYOUNG KIM <lvyest.dev@gmail.com>
2026-02-20 01:12:59 +0900

    feat: books api 구현

--- controller/BookController.js
+++ controller/BookController.js
@@ -1,45 +1,56 @@
 const conn = require('../mariadb'); //db 모듈
 const {StatusCodes} = require('http-status-codes'); //status code 모듈
 
+// (카테고리 별, 신간 여부) 전체 도서 목록 조회
 const allBooks = (req, res)=> {
-    let {category_id} = req.query;
+    let {category_id, news, limit, currentPage} = req.query;
 
-    if(category_id) {
-        let sql = "SELECT * FROM books WHERE category_id = ?";
-        conn.query(sql, category_id,
-            (err, results) => {
-            if(err) {
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end(); 
-            }
+    // limit : page 당 도서 수          ex. 3
+    // currentPage : 현재 몇 페이지 인지. ex. 1, 2, 3...
+    // offset :                       ex. 0, 3, 6, 9, 12...
+    //                                limit * (currentPage - 1)
+    let offset = limit * (currentPage - 1);
 
-            if(results.length){
-                return res.status(StatusCodes.OK).json(results);
-            }
-        
-            else {
-                return res.status(StatusCodes.NOT_FOUND).end();
-            }
-        })
+    let sql = "SELECT * FROM books";
+    let values = [];
+    if(category_id&&news){
+        sql += " WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
+        values = [category_id];
     }
-    else {
-        //(요약된) 전체 도서 리스트 -> 부분적으로 뽑는 건 프론트엔드
-        let sql = "SELECT * FROM books";
-        conn.query(sql, (err, results) => {
-            if(err) {
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end(); 
-            }
+    else if(category_id) {
+        sql += " WHERE category_id = ?";  
+        values = [category_id];
+    }      
+    else if(news) {
+        sql += " WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()"
+    }
 
+    sql += " LIMIT ? OFFSET ?"; //WHERE절 뒤에 있어야함
+    values.push(parseInt(limit), offset);
+
+    // 카테고리 별 시간 조회 
+    conn.query(sql, values,
+        (err, results) => {
+        if(err) {
+            console.log(err);
+            return res.status(StatusCodes.BAD_REQUEST).end(); 
+        }
+
+        if(results.length){
             return res.status(StatusCodes.OK).json(results);
-        })
-    }
+        }
+    
+        else {
+            return res.status(StatusCodes.NOT_FOUND).end();
+        }
+    })
+
 };
 
 const bookDetail = (req, res)=> {
     let {id} = req.params;
 
-    let sql = "SELECT * FROM books WHERE id = ?";
+    let sql = "SELECT * FROM books LEFT JOIN category ON books.category_id = category.id WHERE books.id=?";
     conn.query(sql, id,
         (err, results) => {
         if(err) {
