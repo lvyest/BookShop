4f5c3c07323dccbb50432de3e29d4b85c9eec2e5
KAYOUNG KIM <lvyest.dev@gmail.com>
2026-02-20 15:24:43 +0900

    feat: carts API 구현

--- data.sql
+++ data.sql
@@ -81,5 +81,16 @@ SELECT *,
     LEFT JOIN category ON books.category_id = category.category_id
     WHERE books.id = 1;
 
+//장바구니 담기
+INSERT INTO cartItems(book_id, quantity, user_id) VALUES(1, 1, 1);
 
+//장바구니 아이템 목록 조회
+SELECT cartItems.id, book_id, title, summary, quantity, price 
+FROM cartItems LEFT JOIN books 
+ON cartItems.book_id = books.id;
 
+//장바구니 아이템 조회
+DELETE FROM cartItems WHERE id = ?;
+
+//장바구니에서 선택한(장바구니 도서 id) 아이템 목록 조회 = 선택한 장바구니 상품 목록 조회
+SELECT * FROM Bookshop.cartitems WHERE user_id=1 AND id IN(1,3);
